387226ac4fa5d161736ee741332cdc5b256fb7be
RaveendranathY <ravindranath273@gmail.com>
2022-11-12 15:42:13 +0530

    1. Length of Longest Sub String

--- string/src/LengthOfLongestSubstring.java
+++ string/src/LengthOfLongestSubstring.java
@@ -1,9 +1,36 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class LengthOfLongestSubstring {
 
     public static void main(String[] args){
 
-        System.out.println("Hello world");
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
 
+    }
 
+    public static int lengthOfLongestSubstring(String s){
+        //best example of sliding window problem
+
+        int max_len = 0;
+
+        int start_win = 0;
+        int end_win = 0;
+
+        Set<Character> set = new HashSet<>();
+
+        while(end_win < s.length()){
+            while(set.contains(s.charAt(end_win))){
+                set.remove(s.charAt(start_win));
+                start_win++;
+            }
+
+            set.add(s.charAt(end_win));
+            max_len = Math.max(max_len, set.size());
+            end_win++;
+        }
+
+        return max_len;
     }
 }
