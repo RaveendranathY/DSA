edfec161974fd5c9d3d2b8ddd6af7b4e549574f3
RaveendranathY <ravindranath273@gmail.com>
2022-11-12 15:31:31 +0530

    adding first change

--- string/src/LengthOfLongestSubstring.java
+++ string/src/LengthOfLongestSubstring.java
@@ -2,7 +2,7 @@ public class LengthOfLongestSubstring {
 
     public static void main(String[] args){
 
-        System.out.println("Hello worlfd");
+        System.out.println("Hello world");
 
 
     }
